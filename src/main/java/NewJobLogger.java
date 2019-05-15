a62beae3a2f6bd4dbd8e4cde6e0359901cfbf103
Manuel Tejeda <mtejeda21@gmail.com>
2019-05-15 08:01:32 -0500

    adding unit test

--- src/main/java/NewJobLogger.java
+++ src/main/java/NewJobLogger.java
@@ -1,16 +1,18 @@
+import com.sun.xml.internal.ws.util.StreamUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.logging.Level.INFO;
 
@@ -29,15 +31,15 @@ public class NewJobLogger {
         return get('C');
     }
 
-    public static Loggerr file(Map params) throws IOException {
-        if (map.containsKey('F')) {
+    public static Loggerr file(Map params) throws Exception {
+        if (!map.containsKey('F')) {
             map.put('F', new FileLogger(params));
         }
         return get('F');
     }
 
-    public static Loggerr database(Map params) throws SQLException {
-        if (map.containsKey('D')) {
+    public static Loggerr database(Map params) throws Exception {
+        if (!map.containsKey('D')) {
             map.put('D', new DatabaseLogger(params));
         }
         return get('D');
@@ -62,15 +64,20 @@ public class NewJobLogger {
 
     static class FileLogger implements Loggerr {
         private Logger logger = Logger.getLogger("MyLog");
+        private static final String paramFileFolder = "logFileFolder";
 
-        public FileLogger(Map dbParams) throws IOException {
-            File logFile = new File(dbParams.get("logFileFolder") + "/logFile.txt");
-            if (!logFile.exists()) {
-                logFile.createNewFile();
+        public FileLogger(Map dbParams) throws Exception {
+            if (dbParams.containsKey(paramFileFolder)) {
+                File logFile = new File(dbParams.get(paramFileFolder) + "/logFile.txt");
+                if (!logFile.exists()) {
+                    logFile.createNewFile();
+                }
+
+                FileHandler fh = new FileHandler(dbParams.get(paramFileFolder) + "/logFile.txt");
+                logger.addHandler(fh);
+            } else {
+                throw new Exception();
             }
-
-            FileHandler fh = new FileHandler(dbParams.get("logFileFolder") + "/logFile.txt");
-            logger.addHandler(fh);
         }
 
         @Override
@@ -82,8 +89,14 @@ public class NewJobLogger {
     static class DatabaseLogger implements Loggerr {
 
         private Connection connection;
+        Set<String> requiredKeys = new HashSet<>(Arrays.asList("userName", "password", "dbms", "serverName", "portNumber"));
 
-        public DatabaseLogger (Map dbParams) throws SQLException {
+        public DatabaseLogger (Map dbParams) throws Exception {
+            boolean notValid = requiredKeys.stream().map(key -> dbParams.containsKey(key)).anyMatch(e -> !e);
+            if (notValid) {
+                throw new Exception();
+            }
+
             Properties connectionProps = new Properties();
             connectionProps.put("user", dbParams.get("userName"));
             connectionProps.put("password", dbParams.get("password"));
--- src/test/java/JobLoggerTest.java
+++ src/test/java/JobLoggerTest.java
@@ -1,15 +1,33 @@
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JobLoggerTest {
 
     @Test
-    public void shouldLogFile() throws Exception {
-//        JobLogger jobLogger = new JobLogger(true, false, false, true, false, false, null);
-//        jobLogger.logMessage("text", true, false,false);
-
+    public void shouldLogConsole() throws Exception {
         Loggerr loggerr = NewJobLogger.console(Collections.emptyMap());
         loggerr.message("hello");
+    }
+
+    @Test
+    public void shouldLogFile() throws Exception {
+        Loggerr loggerr = NewJobLogger.file(Collections.singletonMap("logFileFolder", "/Users/manueltejeda/Documents/mate/java/logger/"));
+        loggerr.error("hello");
+    }
+
+    @Test(expected = Exception.class)
+    public void shouldThrowExceptionIfParametersAreInvalid_file() throws Exception {
+        NewJobLogger.file(Collections.emptyMap());
+    }
+
+    @Test(expected = Exception.class)
+    public void shouldThrowExceptionIfParametersAreInvalid_database() throws Exception {
+        Map<String, String> map = new HashMap<>();
+        map.put("userName", "");
+        map.put("someattrb", "");
+        NewJobLogger.database(map);
     }
 }
